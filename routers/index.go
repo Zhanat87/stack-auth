8eae2095041f7254c17208c6dd21bb3cb2551f1e
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2016-10-20 16:29:08 +0600

    fixes

--- routers/index.go
+++ routers/index.go
@@ -1,8 +1,6 @@
 package routers
 import (
-	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
-	"github.com/Zhanat87/stack-auth/common"
 	"github.com/Zhanat87/stack-auth/controllers"
 )
 func SetIndexRoutes(router *mux.Router) *mux.Router {
