5f799e56be36bf3eb795ead7dc44984591a944d8
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2016-10-20 13:56:55 +0600

    fixes

--- controllers/PingController.go
+++ controllers/PingController.go
@@ -2,13 +2,13 @@ package controllers
 
 import (
 	"net/http"
-	"github.com/Zhanat87/stack-auth/common"
+	common "github.com/Zhanat87/stack-auth/common"
 )
 
 func PingHandler(w http.ResponseWriter, r *http.Request) {
-	respondJson("All good. You don't need to be authenticated to call this 2", w)
+	common.respondJson("All good. You don't need to be authenticated to call this 2", w)
 }
 
 func SecuredPingHandler(w http.ResponseWriter, r *http.Request) {
-	respondJson("All good. You only get this message if you're authenticated 2", w)
+	common.respondJson("All good. You only get this message if you're authenticated 2", w)
 }
