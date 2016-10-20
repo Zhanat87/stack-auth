c35450495413e98a7ccd4bcb8d17f51857595c98
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2016-10-20 17:56:35 +0600

    fixes

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	//"strings"
 	"net/http"
-	"encoding/json"
+	//"encoding/json"
 )
 
 //type EnvVarsModel struct {
@@ -20,14 +20,14 @@ func main() {
 	/*
 	@link https://gobyexample.com/environment-variables
 	 */
-	fmt.Println("Something went wrong!", os.Getenv("MYVAR"))
-	fmt.Println()
-	for _, e := range os.Environ() {
-		fmt.Println(e)
-		//pair := strings.Split(e, "=")
-		//fmt.Println(pair[0])
-		//fmt.Println(pair[1])
-	}
+	//fmt.Println("Something went wrong!", os.Getenv("MYVAR"))
+	//fmt.Println()
+	//for _, e := range os.Environ() {
+	//	fmt.Println(e)
+	//	//pair := strings.Split(e, "=")
+	//	//fmt.Println(pair[0])
+	//	//fmt.Println(pair[1])
+	//}
 	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
 		//output, err := json.Marshal(EnvVarsModel{vars: os.Environ(), mongoDbHost: os.Getenv("STACK_MONGODB_PORT_27017_TCP_ADDR"), a: "b"})
 		//if err != nil {
