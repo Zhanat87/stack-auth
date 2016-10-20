5a93a432438e98656b1f3429aa2e56b73f6b14f7
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2016-10-20 15:05:16 +0600

    fixes

--- main.go
+++ main.go
@@ -1,22 +1,26 @@
 package main
+
 import (
 	"log"
 	"net/http"
+
 	"github.com/codegangsta/negroni"
-	"github.com/Zhanat87/stack-auth/common"
+	_ "github.com/Zhanat87/stack-auth/common"
 	"github.com/Zhanat87/stack-auth/routers"
 )
+
 //Entry point of the program
 func main() {
-	// Calls startup logic
-	common.StartUp()
+
+	//common.StartUp() - Replaced with init method
 	// Get the mux router object
 	router := routers.InitRoutes()
 	// Create a negroni instance
 	n := negroni.Classic()
 	n.UseHandler(router)
+
 	server := &http.Server{
-		Addr: common.AppConfig.Server,
+		Addr:    common.AppConfig.Server,
 		Handler: n,
 	}
 	log.Println("Listening...")
