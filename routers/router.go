bfc68b49d86c162dd6834cd48f69a271e4f0422f
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2016-11-04 11:18:06 +0600

    users list

--- routers/router.go
+++ routers/router.go
@@ -1,9 +1,11 @@
 package routers
 import (
 	"github.com/gorilla/mux"
+	"github.com/Zhanat87/stack-auth/controllers"
 )
 func InitRoutes() *mux.Router {
 	router := mux.NewRouter().StrictSlash(false)
+	router.HandleFunc("/test", controllers.GetUsers).Methods("GET")
 	// Routes for the index
 	router = SetIndexRoutes(router)
 	// Routes for the User entity
--- routers/user.go
+++ routers/user.go
@@ -4,7 +4,6 @@ import (
 	"github.com/Zhanat87/stack-auth/controllers"
 )
 func SetUserRoutes(router *mux.Router) *mux.Router {
-	router.HandleFunc("/users", controllers.GetUsers).Methods("GET")
 	router.HandleFunc("/users/register", controllers.Register).Methods("POST")
 	router.HandleFunc("/users/login", controllers.Login).Methods("POST")
 	return router
