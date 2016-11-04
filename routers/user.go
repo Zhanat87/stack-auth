0ec3665a0b92dfc4d311d80fdbdbca33432b8a84
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2016-11-04 11:23:00 +0600

    users list

--- routers/user.go
+++ routers/user.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Zhanat87/stack-auth/controllers"
 )
 func SetUserRoutes(router *mux.Router) *mux.Router {
+	router.HandleFunc("/users", controllers.GetUsers).Methods("GET")
 	router.HandleFunc("/users/register", controllers.Register).Methods("POST")
 	router.HandleFunc("/users/login", controllers.Login).Methods("POST")
 	return router
