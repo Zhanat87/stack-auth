51fb64cb681a34f002307ed5cb7f8a17837bd22e
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2016-11-04 11:11:51 +0600

    users list

--- routers/user.go
+++ routers/user.go
@@ -5,7 +5,7 @@ import (
 )
 func SetUserRoutes(router *mux.Router) *mux.Router {
 	router.HandleFunc("/users", controllers.GetUsers).Methods("GET")
-	router.HandleFunc("/users/register", controllers.Register).Methods("GET")
-	router.HandleFunc("/users/login", controllers.Login).Methods("GET")
+	router.HandleFunc("/users/register", controllers.Register).Methods("POST")
+	router.HandleFunc("/users/login", controllers.Login).Methods("POST")
 	return router
 }
