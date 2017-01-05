29ddfaacf170db8e5a0b1ae5951eeef9d5d9befc
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2017-01-05 10:37:53 +0600

    rabbitmq simple example

--- controllers/indexController.go
+++ controllers/indexController.go
@@ -13,9 +13,29 @@ type EnvVarsModel struct {
 	mongoDbHost string `json:"mongoDbHost"`
 }
 
+type EnvVariablesModel struct {
+	vars []string `json:"vars"`
+}
+
 // Handler for HTTP Get - "/"
 func Index(w http.ResponseWriter, r *http.Request) {
 	res, err := json.Marshal(EnvVarsModel{vars: os.Environ(), mongoDbHost: os.Getenv("STACK_MONGODB_PORT_27017_TCP_ADDR")})
+	if err != nil {
+		common.DisplayAppError(
+			w,
+			err,
+			"An unexpected error has occurred",
+			500,
+		)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+// Handler for HTTP Get - "/test"
+func Test(w http.ResponseWriter, r *http.Request) {
+	res, err := json.Marshal(EnvVariablesModel{vars: os.Environ()})
 	if err != nil {
 		common.DisplayAppError(
 			w,
--- routers/index.go
+++ routers/index.go
@@ -1,10 +1,13 @@
 package routers
+
 import (
 	"github.com/gorilla/mux"
 	"github.com/Zhanat87/stack-auth/controllers"
 )
+
 func SetIndexRoutes(router *mux.Router) *mux.Router {
 	indexRouter := mux.NewRouter()
 	indexRouter.HandleFunc("/index", controllers.Index).Methods("GET")
+	indexRouter.HandleFunc("/test", controllers.Test).Methods("GET")
 	return indexRouter
 }
